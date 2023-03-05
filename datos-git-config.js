ac3c341c5f134e4e52a4f93c5d19b06620ff6c68
rigobersio <rigomartinez@udec.cl>
2023-03-05 19:26:24 -0300

    solo para probar remoteo

---
+++ datos-git-config.js
@@ -0,0 +1,17 @@
+var texto= `
+antes de hacer git init hay que hacer
+git config --global user.name <ingresar name> && user.email <ingresar email>
+lo anterior en ocaciones no funciona en windows y es necesario ingresar la información
+ manualmente en el archivo config dentro de la carpeta .git:
+
+[user]
+
+user.name = name
+user.email= email
+
+lo anterior no parece ser importante en linux. a pesar de que la información no se añade
+en el archivo config si fue ingresada en alguna base de datos
+
+
+`;
+console.log(texto);
