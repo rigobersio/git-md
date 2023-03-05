281957dd476fff3dd089b8a1bea34a46f2f332f8
rigobersio <rigomartinez@udec.cl>
2023-03-05 19:39:55 -0300

    (codeo)_txtblanco_datosconfig_()

--- datos-git-config.js
+++ datos-git-config.js
@@ -1,5 +1,5 @@
 var texto= `
-antes de hacer git init hay que hacer
+antes de hacer git init (o en el caso de una clonación seria antes de un commit) hay que hacer
 git config --global user.name <ingresar name> && user.email <ingresar email>
 lo anterior en ocaciones no funciona en windows y es necesario ingresar la información
  manualmente en el archivo config dentro de la carpeta .git:
@@ -12,6 +12,9 @@ user.email= email
 lo anterior no parece ser importante en linux. a pesar de que la información no se añade
 en el archivo config si fue ingresada en alguna base de datos
 
+además de lo anterior se confirma que se necesita github-cli
 
 `;
+
+
 console.log(texto);
---
+++ protocolocommit.js
@@ -0,0 +1,13 @@
+var texto =`
+
+1. indicar equipo (arch, codeo, minios, win)
+2._ archivo eliminado
+3._archivo nuevo
+4._archivo editado
+5.(nota)
+
+
+
+`;
+
+cosole.log(texto);
