467cb94e664d8f78bdf3da2c270a6cbefeb9a5e7
rigobersio <rigomartinez@udec.cl>
2023-03-05 19:47:11 -0300

    (codeo)_txtblanco_datosconfig_logueo-push()

--- logueo-push-y-clon-github-con-gh.js
+++ logueo-push-y-clon-github-con-gh.js
@@ -8,7 +8,7 @@ GitHub CLI permite gestionar credenciales GitHub y ayuda a clonar y pushear
 	1.1) para arch es sudo pacaman -S github-cli
 
 2) loguearse con GitHub CLI:
-	2.1) en el terminal ejecutar gh aut login y seguir las instrucciones
+	2.1) en el terminal ejecutar gh auth login y seguir las instrucciones
 	2.2) apretar enter si el proceso no avanza
 	2.3) con flechas seleccionar protocolo HTTPS
 	2.4) elegir autentificaciion en Git con GitHub (y)
