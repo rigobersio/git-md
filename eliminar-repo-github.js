a32bba9c0b2be6099a19e92ab51f64e9cfca2d04
rigobersio <rigomartinez@udec.cl>
2023-03-09 16:03:10 -0300

    (win)__eliminar-repo-github_()

---
+++ eliminar-repo-github.js
@@ -0,0 +1,16 @@
+var = texto;
+
+
+texto =`
+1. entrar en el repo a eliminar
+2. ir a settings
+3. ir a danger zona
+4. delate this repository
+5. confirmar escribiendo el nombre del repo y luego con la clave github
+
+
+
+`
+
+
+console.log(texto);
