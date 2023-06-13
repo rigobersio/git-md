47e7a01b621005fd36f93f8fd76ded47a9fce436
rigobersio <rigomartinez@udec.cl>
2023-06-13 09:54:09 -0400

    (mxescritorio)__nuevoParaMxDest_(probando nuevo logeo github para push. gh auth login no es suficiente, se intenta sumando git config --global)

---
+++ nuevoParaMxDest.js
@@ -0,0 +1,9 @@
+este archivo ha sido creado solo para probar el logueo git-hub en git mx escritorio
+
+en este punto se anotaran los resultados  del commit (13-6-2023): la identicacion
+fallo, se procede a hacer otro logueo y otro git add antes del commit
+
+en este punto se anotaran los resultados del 2do intento de commit:se hace un
+ git config --global
+
+en este punto se agregaran los resultados del 3er intento commit:
