583f3227166518720e80636874ce2ff66711cafd
rigobersio <rigomartinez@udec.cl>
2023-06-13 10:09:06 -0400

    (ms escritorio)__nuevoParaMxDes se edito a gitConfigGlobalMasGitHub.js(el problema del commit fue solucionado y como resultado se creo este archivo con info sobre el proceso)

--- gitConfigGlobalMasGithub.js
+++ gitConfigGlobalMasGithub.js
@@ -7,3 +7,14 @@ en este punto se anotaran los resultados del 2do intento de commit:se hace un
  git config --global
 
 en este punto se agregaran los resultados del 3er intento commit:
+exito
+
+hipotesis: commit necesita una identificación del autor del repo antes de hacer
+ un commit, en este punto, aun no tiene que ver mucho o tal vez nada github
+
+en definitiva hay que hacer un git config --global user.mail && user.name
+
+se podria intentar editando el archivo .git/config
+
+**igualmente un logueo github antes de clonar un repo git podria solucionar el
+ entuerto ** PARTIR POR ACÁ!!!
